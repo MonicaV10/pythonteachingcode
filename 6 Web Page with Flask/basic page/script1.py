99b86c126b927b36e1ac2e4901d370c33481635f
MonicaV10 <69943073+MonicaV10@users.noreply.github.com>
2020-10-20 09:05:28 -0400

    flask

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/monica/')
+def monica():
+    return render_template("monica.html")
+    
 if __name__=="__main__":
     app.run(debug=True)
